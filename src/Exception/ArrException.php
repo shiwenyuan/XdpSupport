3aadf4a46e1f51f762fe30846d30c74bf21f6bbb
北行10000 <13341007105@163.com>
2018-08-07 13:32:39 +0800

    init

---
+++ src/Exception/ArrException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shiwenyuan
+ * Date: 2018/8/7 13341007105@163.com
+ * Time: 下午1:11
+ */
+namespace XdpSupport\Exception;
+use Exception;
+
+class ArrException extends Exception
+{
+
+}
---
+++ test/test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shiwenyuan
+ * Date: 2018/8/7 13341007105@163.com
+ * Time: 下午1:28
+ */
+
+require_once __DIR__."/../vendor/autoload.php";
+
+use XdpSupport\Arr;
+$ary = [
+    'name' => 'zhangsan',
+    'pwd' => 'asdf234'
+];
+
+var_dump(Arr::set($ary, 'port', 3306));
+
+var_dump(Arr::get($ary,'port'));
+var_dump(Arr::has($ary, ['port','pwsd']));
